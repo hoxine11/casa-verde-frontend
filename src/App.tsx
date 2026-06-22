f97714411ef4eaea19898cc7f93a9fcaec004027
hoxine11 <hocinebaslimane2003@gmail.com>
2026-06-22 17:59:44 +0100

    console

--- src/App.tsx
+++ src/App.tsx
@@ -292,6 +292,8 @@ export default function App() {
 
   // Handle Cart operators
   const handleAddToCart = (product: Product) => {
+    console.log("ADDING PRODUCT =", product);
+    console.log("SELECTED OPTION =", product.selectedOption);
     setCart((prev) => {
       const exists = prev.find(
         (item) =>
@@ -333,6 +335,7 @@ export default function App() {
     e.preventDefault();
 
     try {
+      console.log("CART =", cart);
       const payload = {
         customerName: checkoutName,
         phone: checkoutPhone,
